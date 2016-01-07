95c534c9591251572d2e20ad38d84bb68895642f
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-01-07 17:22:44 -0500

    Fixed the end tag pretty print issue

--- bootstrap/test-j2m.js
+++ bootstrap/test-j2m.js
@@ -2,12 +2,17 @@ var j2m = require('../src/json-to-markup/j2m.js');
 
 var obj = {
 	molecule: {
+		'@id': 'funmole',
 		atomicWeight: 2,
 		name: 'Helium',
 		type: {
 			'Period': 1,
 			'state': 'gas',
 			'volatility': 'stable'
+		},
+		uses: {
+			'balloons': true,
+			'bouncy houses': false
 		}
 	}
 };
