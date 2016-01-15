fdc01fd10b2a9247ef275ebf8984df6a495a9bbc
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-01-14 22:25:51 -0500

    Some changes

--- bin/debug/j2m-0.0.1.js
+++ bin/debug/j2m-0.0.1.js
@@ -1,221 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* *******************
- * Attr
- */
-function Attr(name, value) {
-	this.name = name;
-	this.value = value;
-}
-
-Attr.prototype.toString = function () {
-	return ' ' + this.name + '="' + this.value + '"';
-}
-
-if (typeof module !== 'undefined') {
-	// node.js export (if we're using node.js)
-	module.exports = Attr;
-}
-
-},{}],2:[function(require,module,exports){
-var Attr = require('./Attr.js');
-
-/* *******************
- * Element
- */
-// Syntax: ele = new Element(tagName) => creates an element with only the tagName specified
-//		   ele = new Element(tagName, child) => creates an element with a tagName and a child
-function Element(tagName, child) {
-	this.tagName = tagName;
-	this.attributes = [];
-	this.children = [];
-
-	if (typeof child !== 'undefined' && child !== null) {
-		if (child instanceof Element) {
-			this.addChild(child);
-		} else {
-			this.addChild(child.toString());
-		}
-	}
-}
-
-Element.prototype.addAttr = function (attr) {
-	if (!(attr instanceof Attr)) {
-		throw new Error('Element.addAttr must be passed an instance of type: Attr');
-	}
-
-	this.attributes.push(attr);
-}
-
-Element.prototype.getNumberedChildElementIndex = function (childElementTagName, index) {
-	var foundIndex = -1;
-	for (var c = 0; c < this.children.length; c++) {
-		var child = this.children[c];
-		if (child.tagName === childElementTagName) {
-			foundIndex++;
-			if (foundIndex === index) {
-				return c;
-			}
-		}
-	}
-	return -1;
-}
-
-// Adds a child element (or plain text)
-// Syntax: this.addChild(childElement) => appends the child element
-//		   this.addChild(childElement, index) => inserts the child element at a specific index
-Element.prototype.addChild = function (childElement, index) {
-	if (!(childElement instanceof Element) &&
-		!(childElement instanceof Array) &&
-		typeof childElement !== 'string') {
-		throw new Error('Element.addChild must be passed an Element instance, Array or a string');
-	}
-
-	if (typeof index === 'undefined') {
-		if (childElement instanceof Array) {
-			for (var v = 0; v < childElement.length; v++) {
-				childElement[v].indexPos = index;
-				this.children.push(childElement[v]);
-			}
-		} else {
-			this.children.push(childElement);
-		}
-	} else {
-		if (childElement instanceof Array) {
-			// this array does a reverse read because of the resorting to be done later on
-			// with elements with index
-			for (var v = childElement.length - 1; v >= 0; v--) {
-				childElement[v].indexPos = index;
-				var childIndex = this.getNumberedChildElementIndex(childElement[v].tagName, index);
-				if (childIndex > -1) {
-					var oldKids = this.children[childIndex].children,
-						oldAttrs = this.children[childIndex].attributes;
-
-					oldKids.forEach(function (item) {
-						childElement[v].children.push(item);
-					});
-					oldAttrs.forEach(function (item) {
-						childElement[v].attributes.push(item);
-					});
-
-					this.children[childIndex] = childElement[v];
-				} else {
-					this.children.push(childElement[v]);
-				}
-			}
-		} else {
-			childElement.indexPos = index;
-			var childIndex = this.getNumberedChildElementIndex(childElement.tagName, index);
-			if (childIndex > -1) {
-				var oldKids = this.children[childIndex].children,
-					oldAttrs = this.children[childIndex].attributes;
-
-				oldKids.forEach(function (item) {
-					childElement.children.push(item);
-				});
-				oldAttrs.forEach(function (item) {
-					childElement.attributes.push(item);
-				});
-
-				this.children[childIndex] = childElement;
-			} else {
-				this.children.push(childElement);
-			}
-		}
-	}
-
-	this.sortChildren();
-}
-
-Element.prototype.sortChildren = function () {
-	var numberedSets = {};
-	for (var c = this.children.length - 1; c >= 0; c--) {
-		var child = this.children[c];
-		if (typeof child.indexPos === 'number') {
-			var arr = numberedSets[child.tagName];
-			if (!arr) {
-				numberedSets[child.tagName] = arr = [];
-			}
-
-			arr.push({
-				actualIndex: c,
-				ele: child
-			});
-
-			// temporarily blank out the element in the children array
-			// so that the elements that match the tagName and have the indexPos
-			// can be reordered
-			this.children[c] = null;
-		}
-	}
-
-	for (var tagName in numberedSets) {
-		var arr = numberedSets[tagName];
-		arr.sort(function (a, b) {
-			return a.ele.indexPos - b.ele.indexPos;
-		});
-
-		var indexes = [];
-		arr.forEach(function (item) {
-			indexes.push(item.actualIndex);
-		});
-
-		indexes.sort();
-
-		for (var i = 0; i < indexes.length; i++) {
-			var child = arr[i].ele;
-			var actualIndex = indexes[i];
-			this.children[actualIndex] = child;
-		}
-	}
-}
-
-Element.prototype.toString = function (indent) {
-	var isIndentable = typeof indent === 'number';
-	var nextIndent = undefined;
-	var indentStr = '';
-	if (isIndentable) {
-		indentStr = '  '.repeat(indent);
-		nextIndent = indent + 1;
-	}
-
-	var str = indentStr + '<' + this.tagName;
-
-	this.attributes.forEach(function (attr) {
-		str += attr.toString();
-	});
-
-	str += '>';
-
-	if (isIndentable && this.children.length > 0 && this.children[0] instanceof Element) {
-		// first child is an element so break to newline
-		str += '\n';
-	}
-
-	this.children.forEach(function (child) {
-		str += child.toString(nextIndent);
-	});
-
-	if (isIndentable && this.children.length > 0 && this.children[0] instanceof Element) {
-		// first child is an element so indent before the end tag
-		str += indentStr;
-	}
-
-	str += '</' + this.tagName + '>';
-
-	if (isIndentable) {
-		str += '\n';
-	}
-
-	return str;
-}
-
-if (typeof module !== 'undefined') {
-	// node.js export (if we're using node.js)
-	module.exports = Element;
-}
-
-},{"./Attr.js":1}],3:[function(require,module,exports){
-/* *******************
  * Token: A token
  */
 
@@ -260,7 +44,7 @@ if (typeof module !== 'undefined') {
 	module.exports = Token;
 }
 
-},{}],4:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 /*
  * This module traverses a lexical tree of tokens and emits specific ones that are subscribed to.
  */
@@ -337,7 +121,7 @@ if (typeof module !== 'undefined') {
 	module.exports = astEmitter;
 }
 
-},{}],5:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 /*
  * This module is the parser for the dot expressions used in the j2m system.
  */
@@ -419,7 +203,7 @@ if (typeof module !== 'undefined') {
 	module.exports = parser;
 }
 
-},{"./Token.js":3,"./parserUtilsExtended.js":7,"./parserUtilsRestricted.js":8}],6:[function(require,module,exports){
+},{"./Token.js":1,"./parserUtilsExtended.js":5,"./parserUtilsRestricted.js":6}],4:[function(require,module,exports){
 var Token = require('./Token.js');
 
 /* *******************
@@ -560,7 +344,7 @@ if (typeof module !== 'undefined') {
 	module.exports = parserCommonFunctions;
 }
 
-},{"./Token.js":3}],7:[function(require,module,exports){
+},{"./Token.js":1}],5:[function(require,module,exports){
 var Token = require('./Token.js'),
 	parserCommonFunctions = require('./parserCommonFunctions.js'),
 	parserUtilsRestricted = require('./parserUtilsRestricted.js');
@@ -1066,7 +850,7 @@ if (typeof module !== 'undefined') {
 	module.exports = parserUtilsExtended;
 }
 
-},{"./Token.js":3,"./parserCommonFunctions.js":6,"./parserUtilsRestricted.js":8}],8:[function(require,module,exports){
+},{"./Token.js":1,"./parserCommonFunctions.js":4,"./parserUtilsRestricted.js":6}],6:[function(require,module,exports){
 var Token = require('./Token.js'),
 	parserCommonFunctions = require('./parserCommonFunctions.js');
 
@@ -1356,7 +1140,223 @@ if (typeof module !== 'undefined') {
 	module.exports = parserUtilsRestricted;
 }
 
-},{"./Token.js":3,"./parserCommonFunctions.js":6}],9:[function(require,module,exports){
+},{"./Token.js":1,"./parserCommonFunctions.js":4}],7:[function(require,module,exports){
+/* *******************
+ * Attr
+ */
+function Attr(name, value) {
+	this.name = name;
+	this.value = value;
+}
+
+Attr.prototype.toString = function () {
+	return ' ' + this.name + '="' + this.value + '"';
+}
+
+if (typeof module !== 'undefined') {
+	// node.js export (if we're using node.js)
+	module.exports = Attr;
+}
+
+},{}],8:[function(require,module,exports){
+var Attr = require('./Attr.js');
+
+/* *******************
+ * Element
+ */
+// Syntax: ele = new Element(tagName) => creates an element with only the tagName specified
+//		   ele = new Element(tagName, child) => creates an element with a tagName and a child
+function Element(tagName, child) {
+	this.tagName = tagName;
+	this.attributes = [];
+	this.children = [];
+
+	if (typeof child !== 'undefined' && child !== null) {
+		if (child instanceof Element) {
+			this.addChild(child);
+		} else {
+			this.addChild(child.toString());
+		}
+	}
+}
+
+Element.prototype.addAttr = function (attr) {
+	if (!(attr instanceof Attr)) {
+		throw new Error('Element.addAttr must be passed an instance of type: Attr');
+	}
+
+	this.attributes.push(attr);
+}
+
+Element.prototype.getNumberedChildElementIndex = function (childElementTagName, index) {
+	var foundIndex = -1;
+	for (var c = 0; c < this.children.length; c++) {
+		var child = this.children[c];
+		if (child.tagName === childElementTagName) {
+			foundIndex++;
+			if (foundIndex === index) {
+				return c;
+			}
+		}
+	}
+	return -1;
+}
+
+// Adds a child element (or plain text)
+// Syntax: this.addChild(childElement) => appends the child element
+//		   this.addChild(childElement, index) => inserts the child element at a specific index
+Element.prototype.addChild = function (childElement, index) {
+	if (!(childElement instanceof Element) &&
+		!(childElement instanceof Array) &&
+		typeof childElement !== 'string') {
+		throw new Error('Element.addChild must be passed an Element instance, Array or a string');
+	}
+
+	if (typeof index === 'undefined') {
+		if (childElement instanceof Array) {
+			for (var v = 0; v < childElement.length; v++) {
+				childElement[v].indexPos = index;
+				this.children.push(childElement[v]);
+			}
+		} else {
+			this.children.push(childElement);
+		}
+	} else {
+		if (childElement instanceof Array) {
+			// this array does a reverse read because of the resorting to be done later on
+			// with elements with index
+			for (var v = childElement.length - 1; v >= 0; v--) {
+				childElement[v].indexPos = index;
+				var childIndex = this.getNumberedChildElementIndex(childElement[v].tagName, index);
+				if (childIndex > -1) {
+					var oldKids = this.children[childIndex].children,
+						oldAttrs = this.children[childIndex].attributes;
+
+					oldKids.forEach(function (item) {
+						childElement[v].children.push(item);
+					});
+					oldAttrs.forEach(function (item) {
+						childElement[v].attributes.push(item);
+					});
+
+					this.children[childIndex] = childElement[v];
+				} else {
+					this.children.push(childElement[v]);
+				}
+			}
+		} else {
+			childElement.indexPos = index;
+			var childIndex = this.getNumberedChildElementIndex(childElement.tagName, index);
+			if (childIndex > -1) {
+				var oldKids = this.children[childIndex].children,
+					oldAttrs = this.children[childIndex].attributes;
+
+				oldKids.forEach(function (item) {
+					childElement.children.push(item);
+				});
+				oldAttrs.forEach(function (item) {
+					childElement.attributes.push(item);
+				});
+
+				this.children[childIndex] = childElement;
+			} else {
+				this.children.push(childElement);
+			}
+		}
+	}
+
+	this.sortChildren();
+}
+
+Element.prototype.sortChildren = function () {
+	var numberedSets = {};
+	for (var c = this.children.length - 1; c >= 0; c--) {
+		var child = this.children[c];
+		if (typeof child.indexPos === 'number') {
+			var arr = numberedSets[child.tagName];
+			if (!arr) {
+				numberedSets[child.tagName] = arr = [];
+			}
+
+			arr.push({
+				actualIndex: c,
+				ele: child
+			});
+
+			// temporarily blank out the element in the children array
+			// so that the elements that match the tagName and have the indexPos
+			// can be reordered
+			this.children[c] = null;
+		}
+	}
+
+	for (var tagName in numberedSets) {
+		var arr = numberedSets[tagName];
+		arr.sort(function (a, b) {
+			return a.ele.indexPos - b.ele.indexPos;
+		});
+
+		var indexes = [];
+		arr.forEach(function (item) {
+			indexes.push(item.actualIndex);
+		});
+
+		indexes.sort();
+
+		for (var i = 0; i < indexes.length; i++) {
+			var child = arr[i].ele;
+			var actualIndex = indexes[i];
+			this.children[actualIndex] = child;
+		}
+	}
+}
+
+Element.prototype.toString = function (indent) {
+	var isIndentable = typeof indent === 'number';
+	var nextIndent = undefined;
+	var indentStr = '';
+	if (isIndentable) {
+		indentStr = '  '.repeat(indent);
+		nextIndent = indent + 1;
+	}
+
+	var str = indentStr + '<' + this.tagName;
+
+	this.attributes.forEach(function (attr) {
+		str += attr.toString();
+	});
+
+	str += '>';
+
+	if (isIndentable && this.children.length > 0 && this.children[0] instanceof Element) {
+		// first child is an element so break to newline
+		str += '\n';
+	}
+
+	this.children.forEach(function (child) {
+		str += child.toString(nextIndent);
+	});
+
+	if (isIndentable && this.children.length > 0 && this.children[0] instanceof Element) {
+		// first child is an element so indent before the end tag
+		str += indentStr;
+	}
+
+	str += '</' + this.tagName + '>';
+
+	if (isIndentable) {
+		str += '\n';
+	}
+
+	return str;
+}
+
+if (typeof module !== 'undefined') {
+	// node.js export (if we're using node.js)
+	module.exports = Element;
+}
+
+},{"./Attr.js":7}],9:[function(require,module,exports){
 /* *******************
  * String extension: repeat(count) -> String
  * This method will repeat this string for a specified count and return the result, leaving this string unchanged.
@@ -1425,8 +1425,8 @@ if (typeof module !== 'undefined') {
 
 },{"./j2mTransformer.js":11,"./markupPrinter.js":12}],11:[function(require,module,exports){
 require('./String-Extensions.js');
-var Attr = require('../expression-parser/Attr.js'),
-	Element = require('../expression-parser/Element.js'),
+var Attr = require('./Attr.js'),
+	Element = require('./Element.js'),
 	objectGraphCreator = require('./objectGraphCreator');
 
 /* *******************
@@ -1587,7 +1587,7 @@ if (typeof module !== 'undefined') {
 	module.exports = j2mTransformer;
 }
 
-},{"../expression-parser/Attr.js":1,"../expression-parser/Element.js":2,"./String-Extensions.js":9,"./objectGraphCreator":13}],12:[function(require,module,exports){
+},{"./Attr.js":7,"./Element.js":8,"./String-Extensions.js":9,"./objectGraphCreator":13}],12:[function(require,module,exports){
 /* *******************
  * markupPrinter
  */
@@ -1731,7 +1731,7 @@ if (typeof module !== 'undefined') {
 	module.exports = objectGraphCreator;
 }
 
-},{"../expression-parser/astEmitter.js":4,"../expression-parser/ep.js":5}]},{},[10]);
+},{"../expression-parser/astEmitter.js":2,"../expression-parser/ep.js":3}]},{},[10]);
 
 if (typeof module !== 'undefined') {
 	// node.js export (if we're using node.js)
--- src/expression-parser/README.md
+++ src/expression-parser/README.md
@@ -1,3 +1,4 @@
 # Expression Parser
 
-This is the parser for the dot expressions that conform to the Usage 1 grammar or the Usages 2 and 3 grammar. This parser is used by the j2m and j2j tools.
+This is the recursive decent parser for the dot expressions that conform to the Usage 1 grammar or the Usages 2 and 3 grammar. This parser is used by the j2m and j2j tools.
+
