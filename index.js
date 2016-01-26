5526c5c086f0e81de5be709648760da43eb2731b
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-01-25 22:23:07 -0500

    Fixed require issue

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /*
  * The main module for friedjuju
  */
+var path = require('path');
 if (typeof module !== 'undefined') {
 	// node.js export (if we're using node.js)
 	module.exports = {
-		j2m: require('src/json-to-markup/j2m.js'),
-		j2j: require('src/json-to-json/j2j.js'),
-		j2f: require('src/json-to-function/j2f.js')
+		j2m: require(path.join(__dirname, 'src/json-to-markup/j2m.js')),
+		j2j: require(path.join(__dirname, 'src/json-to-json/j2j.js')),
+		j2f: require(path.join(__dirname, 'src/json-to-function/j2f.js'))
 	};
 }
